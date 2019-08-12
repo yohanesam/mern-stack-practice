c70c8e8337ec2b89e97f39bbb75a41e96e6f7384
aryo <aryo100@gmail.com>
2019-08-12 12:01:57 +0700

    add User Registeration Token

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+    // Get token from header
+    const token = req.header('x-auth-token');
+
+    // check if not token
+    if (!token) {
+        return res.status(401).json({ msg: 'Notoken, authorization denied' });
+    } 
+    
+    // Verify token
+    try {
+        const decode = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decode.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid '});
+    }
+}
